d545e5bed317ec9067cf75dd4cf854269467e99c
vasiliy <poteychuk87@mail.ru>
2022-05-19 23:43:35 +0300

    finish homework 1605

---
+++ home.js
@@ -0,0 +1,41 @@
+"use strict"
+
+// let a = +prompt ('enter first number');
+// let b = +prompt ('enter second number');
+// let c = +prompt ('enter third number');
+// if( a > 10 && b > 10 && c > 10 && a % 3 == 0 && b % 3 == 0){
+//     alert ('yes');
+// }else{
+//     alert ('no');
+// }// первая задача
+
+// let n = +prompt('enter number');
+// if (n >= 2 && n <= 5){
+//     n=n*10;
+// }else if (n >=7 && n <= 40){
+//     n=n/100;
+// }else if (n <=0 && n >= 3000){
+//     n=n*3;
+// }else(n = n)
+// console.log(n);//вторая задача
+
+// let p = 1;
+// for (let n = 5; n < 111; n++){
+//     p = n * p
+//     console.log(p);
+// }//3 задача
+
+// for (let n = 1; n < 10 ; n++){
+//     if (n%2){
+//         console.log('*******');
+//     }else{
+//         console.log('****');
+//     }
+// }//4 задача
+
+// let n = +prompt('enter number');
+// let b = 0;
+// for (let a = 1; a <= n; a++){
+//     b = b + a;
+//     console.log(b);
+// }//5 задача
